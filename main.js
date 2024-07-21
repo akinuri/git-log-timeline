dc0d47ad8669bb1ffe569c3a47d990cc86e702d8
Noreh AD <akinoreh@gmail.com>
2024-07-21 22:27:05 +0300

    group by date

--- main.js
+++ main.js
@@ -4,6 +4,25 @@ let outputArea = qs("#output-area");
 
 on(parseBtn, "click", () => {
     let parsed = CommitsParser.parse(logInput.value);
-    outputArea.textContent = JSON.stringify(parsed, null, "    ");
+    
+    let groupedByDate = groupBy(parsed, commit => commit.date.split(" ")[0])
+    
+    outputArea.textContent = JSON.stringify(groupedByDate, null, "    ");
 });
 
+function groupBy(array, prop) {
+    let grouped = {};
+    for (const item of array) {
+        let key;
+        if (typeof prop == "function") {
+            key = prop(item);
+        }
+        if (key) {
+            if (!(key in grouped)) {
+                grouped[key] = [];
+            }
+            grouped[key].push(item);
+        }
+    }
+    return grouped;
+}
